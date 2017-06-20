35738cc3247f0bfdbc2d796ecfe7f57a68fe8c69
faiz siddiqui <faiz.techno20@gmail.com>
2017-06-20 22:10:01 +0530

    My project setup completed success

---
+++ shoppingbackends/src/main/java/com/faiz/spring/shoppingbackends/App.java
@@ -0,0 +1,13 @@
+package com.faiz.spring.shoppingbackends;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
